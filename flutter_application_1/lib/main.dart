166b9c22b4862a63dc0949b02c467d5d90ea2de4
Devpooh <109036814+ppooh1@users.noreply.github.com>
2024-01-11 14:27:17 +0700

    1

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+    );
+  }
+}
+
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() {
+    return _CalculatorState();
+  }
+}
+
+class _CalculatorState extends State<Calculator> {
+  final TextEditingController firstNumberController = TextEditingController();
+  final TextEditingController secondNumberController = TextEditingController();
+  String output = '';
+
+  void plusFunction() {
+    double firstNumber = double.parse(firstNumberController.text);
+    double secondNumber = double.parse(secondNumberController.text);
+    double result = firstNumber + secondNumber;
+    setState(() {
+      output = result.toString();
+    });
+  }
+
+  void minusFunction() {
+    double firstNumber = double.parse(firstNumberController.text);
+    double secondNumber = double.parse(secondNumberController.text);
+    double result = firstNumber - secondNumber;
+    setState(() {
+      output = result.toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: firstNumberController,
+              decoration: InputDecoration(hintText: 'Enter first number'),
+              textAlign: TextAlign.center,
+            ),
+            TextField(
+              controller: secondNumberController,
+              decoration: InputDecoration(hintText: 'Enter second number'),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 16), // Add 16px of vertical space
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: plusFunction,
+                  child: Text('+'),
+                ),
+                SizedBox(width: 16), // Add 16px of horizontal space
+                ElevatedButton(
+                  onPressed: minusFunction,
+                  child: Text('-'),
+                ),
+              ],
+            ),
+            SizedBox(height: 16), // Add 16px of vertical space
+            Text(
+              'Output: $output',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
